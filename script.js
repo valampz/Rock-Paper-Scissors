fdc9ffd2d526e0cebf02bc1ce07a14c71757d583
Tazim Chowdhury <tazimezazchy@gmail.com>
2024-08-25 00:16:54 +0600

    add working game

--- script.js
+++ script.js
@@ -7,13 +7,72 @@ function getComputerChoice(){
 }
 
 function getHumanChoice(){
-    let userMove = window.prompt('rock, paper or scissor?');
+    let userMove = window.prompt('Rock, Paper or Scissor?');
 
-    if(arr.includes(userMove)){
+    if(arr.includes(userMove.toLowerCase())){
         return userMove;
     }
     else{
-        return `Enter a valid input in lower-case letters`;
+        return `Enter a valid input`;
     }
 }
 
+
+function playRound(humanChoice, computerChoice){
+   
+    if (humanChoice === 'rock'){
+        switch (computerChoice){
+            case 'paper':
+                humanScore--;
+                computeScore++;
+                return 'You lose! Paper beats Rock';
+                break;
+            case 'scissor':
+                humanScore++;
+                computeScore--;
+                return 'You win! Rock beats Scissor';
+                break;
+            default:
+                return 'Nobody wins!';
+        }
+    }
+
+    else if (humanChoice === 'paper'){
+        switch (computerChoice){
+            case 'rock':
+                humanScore++;
+                computeScore--;
+                return 'You win! Paper beats Rock';
+                break;
+            case 'scissor':
+                humanScore--;
+                computeScore++;
+                return 'You lose! Scissor beats Paper';
+                break;
+            default:
+                return 'Nobody wins!';
+        }
+    }
+
+    else if (humanChoice === 'scissor'){
+        switch (computerChoice){
+            case 'paper':
+                humanScore++;
+                computeScore--;
+                return 'You win! Scissor beats Paper';
+                break;
+            case 'rock':
+                humanScore--;
+                computeScore++;
+                return 'You lose! Rock beats Scissor';
+                break;
+            default:
+                return 'Nobody wins!';
+        }
+    }
+}
+
+const humanChoice = getHumanChoice().toLowerCase();
+const computerChoice = getComputerChoice();
+
+console.log(playRound(humanChoice, computerChoice));
