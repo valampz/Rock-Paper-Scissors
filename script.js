4d03d09d983ea1fadfe3c6777f565d32cc368c15
Tazim Chowdhury <tazimezazchy@gmail.com>
2024-08-24 23:31:07 +0600

    add human choice function

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function getComputerChoice(){
-    let arr = ["Rock", "Paper", "Scissor"];
+const arr = ["rock", "paper", "scissor"];
+let computeScore = 0;
+let humanScore = 0;
 
+function getComputerChoice(){
     return arr[Math.floor(Math.random() * 3)];    
+}
+
+function getHumanChoice(){
+    let userMove = window.prompt('rock, paper or scissor?');
+
+    if(arr.includes(userMove)){
+        return userMove;
+    }
+    else{
+        return `Enter a valid input in lower-case letters`;
+    }
 }
 
